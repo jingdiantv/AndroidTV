05fde8396f5209da297ed90de04f41683cd41fee
tuandv4 <dvt1405@gmail.com>
2023-06-12 17:53:00 +0700

    Remove add test default iptv channel

--- app/src/main/java/com/kt/apps/media/xemtv/ui/main/MainActivity.kt
+++ app/src/main/java/com/kt/apps/media/xemtv/ui/main/MainActivity.kt
@@ -4,16 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.kt.apps.core.base.BaseActivity
-import com.kt.apps.core.extensions.ExtensionsConfig
-import com.kt.apps.core.logging.Logger
 import com.kt.apps.core.storage.local.RoomDataBase
 import com.kt.apps.media.xemtv.BuildConfig
 import com.kt.apps.media.xemtv.R
 import com.kt.apps.media.xemtv.databinding.ActivityMainBinding
 import com.kt.apps.media.xemtv.ui.TVChannelViewModel
 import com.kt.apps.media.xemtv.ui.extensions.ExtensionsViewModel
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
 /**
@@ -26,10 +22,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     @Inject
     lateinit var roomDataBase: RoomDataBase
-
-    private val disposable by lazy {
-        CompositeDisposable()
-    }
 
     private val tvChannelViewModel by lazy {
         ViewModelProvider(this, factory)[TVChannelViewModel::class.java]
@@ -47,18 +39,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     override fun initAction(savedInstanceState: Bundle?) {
-        if (BuildConfig.DEBUG) {
-            disposable.add(
-                roomDataBase.extensionsConfig()
-                    .insert(ExtensionsConfig.test)
-                    .subscribeOn(Schedulers.io())
-                    .subscribe({
-                        Logger.e(this, message = "Complete")
-                    }, {
-                        Logger.e(this, exception = it)
-                    })
-            )
-        }
 
         if (BuildConfig.isBeta) {
             extensionsViewModel.insertDefaultSource()
